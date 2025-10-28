c00455f1da82285c0c419f1e6df55ac3edc67a14
sumedhaKl <142531637+sumedhaKl@users.noreply.github.com>
2025-10-27 23:41:35 -0700

    Update Strings2.java

--- Strings2.java
+++ Strings2.java
@@ -1,21 +1,22 @@
 import java.util.Scanner; 
-import java.text.DecimalFormat; 
 
 public class Strings2{ 
     public static void main(String[] args){ 
         Scanner input = new Scanner(System.in);
         System.out.print("Enter employee's name: ");
-        char employeeName = input.nextChar(); 
+        int employeeName = input.nextInt(); 
 
         int numberOfHoursWorkedInAWeek = 0; 
         double hourlyPayRate = 0; 
-        double federalTaxWithholdingRate = 0; 
-        float stateTaxWithholdingRate = 0; 
+        int federalTaxWithholdingRate = 0; 
+        int stateTaxWithholdingRate = 0;
+        int totalDeduction = federalTaxWithholdingRate + stateTaxWithholdingRate; 
+        int grossPay = 100;
+        int netPay = grossPay-totalDeduction;
         System.out.print("Enter number of hours worked in a week:"); 
         System.out.print("Enter hourly pay rate;"); 
         System.out.print("Enter federal tax withholding rate:"); 
         System.out.print("Enter state tax withholding rate:"); 
-        System.out.println(employeeName + numberOfHoursWorkedInAWeek + hourlyPayRate + federalTaxWithholdingRate + stateTaxWithholdingRate); 
-        return; 
+        System.out.println("Employee Name:" + employeeName + "Hours Worked:" + numberOfHoursWorkedInAWeek + "Pay Rate:" + hourlyPayRate + grossPay+ "Federal Withholding" + federalTaxWithholdingRate + "State Withholding:" + stateTaxWithholdingRate + "Total Deduction:" + totalDeduction + "Net Pay:" + netPay); 
     } 
 }
