a1bc64207140f77560d324da500aad083add9752
sumedhaKl <142531637+sumedhaKl@users.noreply.github.com>
2025-10-31 20:07:21 -0700

    Update Strings2.java

--- Strings2.java
+++ Strings2.java
@@ -6,18 +6,20 @@ class Strings2{
             System.out.print("Enter employee's name: ");
             String employeeName = input.nextLine();
             
-            int grossPay = 100;
+            double grossPay = 100;
             System.out.print("\nEnter number of hours worked in a week:");
-            int numberOfHoursWorkedInAWeek = input.nextInt();
+            double numberOfHoursWorkedInAWeek = input.nextDouble();
             System.out.print("\nEnter hourly pay rate:");
             double hourlyPayRate = input.nextDouble();
             System.out.print("\nEnter federal tax withholding rate:");
             double federalTaxWithholdingRate = input.nextDouble();
+            federalTaxWithholdingRate = federalTaxWithholdingRate * 100;
             System.out.print("\nEnter state tax withholding rate:");
             double stateTaxWithholdingRate = input.nextDouble();
+            stateTaxWithholdingRate = stateTaxWithholdingRate * 100;
             double totalDeduction = federalTaxWithholdingRate + stateTaxWithholdingRate;
             double netPay = grossPay-totalDeduction;
-            System.out.println("\nEmployee Name:" + employeeName + "\nHours Worked:" + numberOfHoursWorkedInAWeek + "\nPay Rate:" + hourlyPayRate + "\nGross Pay:" + grossPay+ "\nDeductions: \nFederal Withholding" + federalTaxWithholdingRate + "\nState Withholding:" + stateTaxWithholdingRate + "\nTotal Deduction:" + totalDeduction + "\nNet Pay" + netPay);
+            System.out.println("\nEmployee Name: " + employeeName + "\nHours Worked: " + numberOfHoursWorkedInAWeek + "\nPay Rate: " + hourlyPayRate + "\nGross Pay: " + grossPay+ "\nDeductions: \nFederal Withholding: " + federalTaxWithholdingRate + "\nState Withholding: " + stateTaxWithholdingRate + "\nTotal Deduction: " + totalDeduction + "\nNet Pay: " + netPay);
         }
     } 
 }
